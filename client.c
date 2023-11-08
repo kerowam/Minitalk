fa8a6ddaf40136ed318f2a31174a7a72e64dad79
gfredes- <gfredes-@student.42malaga.com>
2023-11-08 20:00:56 +0100

    Checking for arguments and empty new ft added to client

--- client.c
+++ client.c
@@ -6,13 +6,35 @@
 /*   By: gfredes- <gfredes-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 18:13:19 by gfredes-          #+#    #+#             */
-/*   Updated: 2023/11/08 18:28:52 by gfredes-         ###   ########.fr       */
+/*   Updated: 2023/11/08 19:58:52 by gfredes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
+	int	pid;
+	int	i;
+
+	if (argc != 3)
+	{
+		ft_putstr_fd("Something goes wrong!\n", 1);
+		ft_putstr_fd("Indications of the program: ./client <PID> <String>", 1);
+		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	if (!ft_isdigit(pid))
+	{
+		ft_putstr_fd("Wrong PID! It have to be a number.\n", 1);
+		return (2);
+	}
+	i = 0;
+	while (argv[2][i] != '\0')
+	{
+		ft_sending_bits(pid, argv[2][i]);
+		i++;
+	}
+	ft_sending_bits(pid, '\n');
 	return (0);
 }
